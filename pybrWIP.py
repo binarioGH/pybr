c547a1db597563cc6c0e46666e61db9cf23e8614
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-10-19 20:52:45 -0500

    Re-estructurando todo

--- pybrWIP.py
+++ pybrWIP.py
@@ -1,60 +1,45 @@
 #-*-coding: utf-8-*-
 from sys import argv
-def echo(*strs):
-	for arg in strs:
-		print(arg)
+from os import system, getcwd
+def interactiveinterpreter():
+	print("PYBR es un lenguaje de programación que continúa en desarrollo.")
+	cmd = ""
+	commands = {"cls":"cls()", "pwd": "pwd()"}
+	while cmd != "exit":
+		cmd = input(">>>>")
+		if cmd in commands:
+			cmd = commands[cmd]
+		try:
+		    if cmd[:11] == "interpreter":
+		    	continue
 
-def ret(i=1,*nums):
-	t = nums[0]
-	for n in nums[1:]:
-		t += n*i
-	return t
+		except:
+			pass
+		try:
+			exec(cmd)
+		except Exception as e:
+			print(e)
+	exit()
+def cls():
+	system("cls")
+def pwd():
+	print(getcwd())
 
-def substract(*nums):
-	r = ret(-1, *nums)
-	return r
-	
-def plus(*nums):
-	r = ret(1, *nums)
-	return r
+def interpreter(file):
+	try:
+		f = open(file, "r")
+	except Exception as e:
+		print(e)
+		exit()
+	for l in f:
+		exec(l)
 
-
-def printvars():
-	print(dv)
-
-def vars(l):
-	count = 0
-
-	for char in l:
-		if char == "=":
-			mn = count
-			mx = count + 1
-			break
-		else:
-			count += 1
-	return (mn, mx)
 
 if __name__ == '__main__':
-	dv = {}
-	try:
-		file = open(argv[1], "r")
-	except Exception as e1:
-		print(e1)
-		exit()
+	if len(argv) == 1:
+		interactiveinterpreter()
 	else:
-		code = file.readlines()
-		for line in code:
-			if "=" in code:
-				mm = vars(line)
-				dv[str(line[:mm[0]])] = line[mm[1]:]
-			else: 
-				try:
-					exec(line)
-				except Exception as e2:
-					print(e2)
-					exit()
-				 
-
+		interpreter(argv[1])
 
 
 
